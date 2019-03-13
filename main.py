FIXING: There is a problem with autograd during backpropagation, as it seems the problem is related with in-place operations. The output tensor need to be on the same memory position and for it, in-place operations must be proceeded. Eg. in-place ------> x=/2     ### a = a.reshape(.) ### x.add_(y)       not inplace --> x = x/2 ###  a.reshape(.)      ### x = x+y

main.py
@@ -18,7 +18,7 @@ import utils.transformations as tsfrm
 
 from torch import optim
 from utils.logger import Logger
-from nets.unet import Unet2
+from nets.unet import InstSegNet, Unet2
 from utils.datasets import OvaryDataset
 from utils.losses import DiceLoss, DiscriminativeLoss
 from train import Training
@@ -54,8 +54,10 @@ if __name__ == '__main__':
     logger = Logger('./logs/' + train_name + '/')
 
     # Load Unet
-    model = Unet2(n_channels=1, n_classes=[3,2])
-    #print(net)
+    n_features = 2
+    #model = Unet2(n_channels=1, n_classes=3) 
+    model = InstSegNet(n_channels=1, n_features=n_features)
+    print(model)
 
     # Load CUDA if exist
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -72,7 +74,8 @@ if __name__ == '__main__':
 
     # Training Parameters
     optmizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0005)
-    loss_function = DiscriminativeLoss(n_features=2) #DiceLoss() # nn.CrossEntropyLoss()
+    #loss_function = DiceLoss()
+    loss_function = DiscriminativeLoss(n_features=n_features)
 
     # Run training
     training = Training(model, device, dataset_train, dataset_val,

train.py
@@ -55,7 +55,8 @@ class Training:
         for batch_idx, sample in enumerate(data_loader_train):
             # Load data
             image = sample['image']
-            gt_mask = sample['follicle_instances'] #'gt_mask']
+            #gt_mask = sample['gt_mask'] 
+            gt_mask = sample['follicle_instances']
             # Active GPU train
             if torch.cuda.is_available():
                 self.model = self.model.to(self.device)
@@ -84,7 +85,10 @@ class Training:
                 ref_pred_train = pred_masks[0,...]
 
             # Calculate loss for each batch
+            #loss = self.criterion(pred_masks, groundtruth)
+            print(pred_masks)
             loss = self.criterion(pred_masks, groundtruth)
+            print(loss)
             loss_train_sum += len(image) * loss.item()
 
             # Update weights

utils/losses.py
@@ -9,7 +9,7 @@ Created on Wed Fev 27 18:37:58 2019
 
 import torch
 import torch.nn as nn
-
+from torch.autograd import Variable
 
 class DiceLoss(nn.Module):
     '''
@@ -95,13 +95,13 @@ class DiscriminativeLoss(nn.Module):
         unique_id = correct_label.expand(self.n_features, self.height * self.width).long() # expected index of each pixel
         
          # Get sum by instance
-        segmented_sum = torch.zeros(self.n_features, num_instances).scatter_add(1, unique_id, reshaped_pred)
+        segmented_sum = torch.zeros(self.n_features, num_instances).scatter_add_(1, unique_id, reshaped_pred)
         # Mean of each instance in each feature layer
         mu = torch.div(segmented_sum, counts)
 
         return num_instances, counts, unique_id, mu
 
-    
+
     def _variance_term(self, mu, num_instances, unique_id, counts, reshaped_pred):
         ''' l_var  - intra-cluster distance '''
 
@@ -109,15 +109,12 @@ class DiscriminativeLoss(nn.Module):
         mu_expand = torch.gather(mu, 1, unique_id)
 
         # Calculate intra distance
-        distance = mu_expand - reshaped_pred
-        distance = torch.norm(distance, dim=0) - self.delta_v    # apply delta_v
-        distance = torch.clamp(distance, 0., distance.max())**2 # max(0,x)
-        distance.reshape(1,len(distance))
-
-        l_var = torch.zeros(1, num_instances).scatter_add(1, unique_id[0].reshape(1, self.height * self.width), distance.reshape(1, self.height * self.width))
-        l_var = l_var / counts
-        l_var = l_var.sum() / num_instances
-        print(l_var)
+        distance = torch.clamp(torch.norm(mu_expand - reshaped_pred, dim=0) - self.delta_v, 0., 10000)**2 # max(0,x)   # apply delta_v
+        distance.reshape(1,len(distance)).contiguous()
+        
+        l_var = torch.zeros(1, num_instances).scatter_add_(1, unique_id[0].reshape(1, self.height * self.width), distance.reshape(1, self.height * self.width))
+        l_var /= counts / num_instances
+        l_var.sum() 
 
         return l_var
         
@@ -128,18 +125,22 @@ class DiscriminativeLoss(nn.Module):
         # Calculate inter distance
         mu_sdim = mu.reshape(mu.shape[1] * mu.shape[0]) # reshape to apply meshgrid
         mu_x, mu_y = torch.meshgrid(mu_sdim, mu_sdim)
-        aux_x = mu_x[:,:num_instances].reshape(self.n_features, num_instances, num_instances)#.permute(1,2,0)
+        aux_x = mu_x[:,:num_instances].reshape(self.n_features, num_instances, num_instances)
         aux_y = mu_y[:num_instances, :].reshape(num_instances, self.n_features, num_instances).permute(1,0,2)
         # Calculate differece interclasses
         mu_diff = aux_x - aux_y
         mu_diff = torch.norm(mu_diff,dim=0)
         # Use a matrix with delt_d to calculate each difference
         aux_delta_d = 2 * self.delta_d * (torch.ones(mu_diff.shape) - torch.eye(mu_diff.shape[0])) # ignore diagonal (C_a = C_b)
-        l_dist = aux_delta_d - mu_diff
-        l_dist = torch.clamp(l_dist, 0., l_dist.max())**2 # max(0,x)
+        aux_delta_d = Variable(aux_delta_d)
+        l_dist = torch.clamp(aux_delta_d - mu_diff, 0., 10000)**2 # max(0,x)
+        print(id(l_dist))
         # sum / C(C-1)
-        l_dist = l_dist.sum() / num_instances / (num_instances - 1)
-        print(l_dist)
+        l_dist /= num_instances / (num_instances - 1)
+        print(id(l_dist))
+        l_dist.sum() 
+        print(id(l_dist))
+        #print(l_dist)
 
         return l_dist
 
@@ -149,7 +150,7 @@ class DiscriminativeLoss(nn.Module):
 
         l_reg = torch.norm(mu, dim=0)   # norm
         l_reg = l_reg.sum() / num_instances # sum and divide
-        print(l_reg)
+        #print(l_reg)
         
         return l_reg
 
@@ -161,8 +162,7 @@ class DiscriminativeLoss(nn.Module):
         correct_label.long()
 
         # Prediction
-        pred = torch.rand(1, self.height*self.width, self.n_features)
-        reshaped_pred = pred.reshape(self.n_features, self.height*self.width)
+        reshaped_pred = pred.reshape(self.n_features, self.height*self.width).contiguous()
 
         # Count instances
         num_instances, counts, unique_id, mu = self._sort_instances(correct_label, reshaped_pred)
@@ -176,7 +176,7 @@ class DiscriminativeLoss(nn.Module):
 
         # Loss
         loss = self.alpha * l_var + self.beta *  l_dist + self.gamma * l_reg
-        print(loss)
+        #print(loss)
 
         return loss, l_var, l_dist, l_reg
 
@@ -185,26 +185,20 @@ class DiscriminativeLoss(nn.Module):
 
         # Adjust data - CHECK IF NECESSARY
         self.batch_size, self.height, self.width = target.shape
-        
-        out_lvar = torch.zeros(self.batch_size)
-        out_ldist = torch.zeros(self.batch_size)
-        out_lreg = torch.zeros(self.batch_size)
-        out_loss = torch.zeros(self.batch_size)
+
+        loss_list = []
 
         for i in range(self.batch_size):
 
-            pred = prediction[i,...]
-            tgt = target[i,...]
+            pred = prediction[i,...].contiguous()
+            tgt = target[i,...].contiguous()
 
             loss, l_var, l_dist, l_reg = self._discriminative_loss(pred, tgt)
 
-            out_lvar[i] = l_reg
-            out_ldist[i] = l_var
-            out_lreg[i] = l_dist
-            out_loss[i] = loss
-            
-        return  out_loss.sum() / self.batch_size, \
-                out_lvar.sum() / self.batch_size, \
-                out_ldist.sum() / self.batch_size, \
-                out_lreg.sum() / self.batch_size
+            loss_list.append(loss)
+
+        out_loss = torch.stack(loss_list)
+        out_loss = torch.sum(out_loss)
+                        
+        return  out_loss
                 