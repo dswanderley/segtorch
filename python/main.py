c17d37b7f0689d14180956456bfa10cab318e905
ARGONATH\Diego <dswanderley@gmail.com>
2019-03-29 18:56:35 +0000

    FIXED: Default data path was wrong.

--- python/main.py
+++ python/main.py
@@ -152,7 +152,7 @@ if __name__ == '__main__':
                            tsfrm.RandomAffine(90, translate=(0.15, 0.15), scale=(0.75, 1.5), resample=3, fillcolor=0)
                            ])
     # Dataset definitions
-    dataset_train = OvaryDataset(im_dir='../dataset/im/train_/',gt_dir='../dataset/gt/train/',  imap=interaction, clahe=clahe, transform=transform)
+    dataset_train = OvaryDataset(im_dir='../dataset/im/train/',gt_dir='../dataset/gt/train/',  imap=interaction, clahe=clahe, transform=transform)
     dataset_val =   OvaryDataset(im_dir='../dataset/im/val/',   gt_dir='../dataset/gt/val/',    imap=interaction, clahe=clahe)
     dataset_test =  OvaryDataset(im_dir='../dataset/im/test/',  gt_dir='../dataset/gt/test/',   imap=interaction, clahe=clahe)
 
