fe2dcf47ca5fcef0a670e01ab1e4970871f7b5ad
ARGONATH\Diego <dswanderley@gmail.com>
2019-03-29 14:20:51 +0000

    CHANGE: New argument input, using tags.

--- python/main.py
+++ python/main.py
@@ -8,8 +8,10 @@ Created on Wed Mar 03 10:39:30 2019
 """
 
 import os
+import ast
 import sys
 import time
+import argparse
 import torch
 import torchvision
 
@@ -47,50 +49,83 @@ def gettrainname(name):
 # Main calls
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser(description="PyTorch U-net training and prediction.")
+    parser.add_argument('--net', type=str, default='unet2',
+                        choices=['unet2', 'unet'],
+                        help='network name (default: unet2)')
+    parser.add_argument('--epochs', type=int, default=1,
+                        help='number of epochs (default: 1)')
+    parser.add_argument('--batch_size', type=int, default=3,
+                        help='batch size (default: 3)')
+    parser.add_argument('--dataset', type=str, default='ovarian',
+                        choices=['ovarian'],
+                        help='select dataset, it also defines the input depth and the output classes (default: ovarian)')
+    parser.add_argument('--multitask', type=str, default='none',
+                        choices=['none', 'ovary', 'follicle', 'both'],
+                        help='select dataset (default: ovarian)')
+    parser.add_argument('--opt', type=str, default='adam',
+                        choices=['adam', 'sgd'],
+                        help='optmization process (default: adam)')
+    parser.add_argument('--loss', type=str, default='dsc',
+                        choices=['dice', 'discriminative', 'crossentropy'],
+                        help='loss function (default: dice)')
+    parser.add_argument('--clahe', type=bool, default=False,
+                        help='whether to use adaptive histogram equalization (default: False)')
+    parser.add_argument('--interaction', type=bool, default=False,
+                        help='whether to use interaction points (default: False)')
+    parser.add_argument('--bilinear', type=bool, default=False,
+                        help='whether to use bilinear upsampling should be used instead of Transpose Conv. (default: False)')
+
+
+    # Parse input data
+    args = parser.parse_args()
+
     # Input parameters
-    n_epochs = 1
-    batch_size = 3
-    opt = 'adam'
-    loss = 'dsc'
-    network_name = 'Unet2'
-    bilinear = False
-    clahe = False
-    interaction = False
-    in_channels = 1
-             
-    if(len(sys.argv)>1):
-        n_epochs = int(sys.argv[1])
-    print('epochs:', n_epochs)
-    
-    if(len(sys.argv)>2):
-        batch_size = int(sys.argv[2])
-    print('batch size:', batch_size)
+    n_epochs = args.epochs
+    batch_size = args.batch_size
+    dataset_name = args.dataset
+    opt = args.opt
+    loss = args.loss
+    net_type = args.net
+    bilinear = args.bilinear
+    clahe = args.clahe
+    interaction = args.interaction
+    multitask = args.multitask
 
-    if(len(sys.argv)>3):
-        opt = str(sys.argv[3])
-    print('opt:', opt)
-    
-    if(len(sys.argv)>4):
-        loss = str(sys.argv[4])
-    print('loss:', loss)
+    network_name = net_type
 
-    if(len(sys.argv)>5):
-        network_name = str(sys.argv[5])
-    print('net name:', network_name)
-    
-    if 'b' in network_name:
-        bilinear = True
-    print('bilinear:', bilinear)
+    # Manage network input
+    if dataset_name == 'ovarian':
+        in_channels = 1
+    else:
+        in_channels = 1
 
     if clahe:
         in_channels += 1
-    print('clahe:', clahe)
 
-    if 'i' in network_name:
+    if interaction:
         interaction = [1., 0.5]
         in_channels += 1
-    print('interaction:', interaction)
+        network_name = 'i' + network_name
 
+    if multitask == 'ovarian':
+        n_classes = [3,2]
+        network_name += '_ovar'
+    elif multitask == 'follicle':
+        n_classes = [3,2]
+        network_name += '_foll'
+    elif multitask == 'both':
+        n_classes = [3,2,2]
+        network_name += '_both'
+    else:
+        n_classes = 3
+
+    print(network_name)
+    print('output classes:', n_classes)
+    print('epochs:', n_epochs)
+    print('batch size:', batch_size)
+    print('optmization:', opt)
+    print('loss funcion:', loss)    
     print('---------------------------')
     print('')
 
@@ -101,7 +136,7 @@ if __name__ == '__main__':
     logger = Logger('../logs/' + train_name + '/')
 
     # Load Unet
-    model = Unet2(n_channels=in_channels, n_classes=3, bilinear=bilinear)
+    model = Unet2(n_channels=in_channels, n_classes=n_classes, bilinear=bilinear)
     #print(net)
 
     # Load CUDA if exist
@@ -113,9 +148,9 @@ if __name__ == '__main__':
                            tsfrm.RandomAffine(90, translate=(0.15, 0.15), scale=(0.75, 1.5), resample=3, fillcolor=0)
                            ])
     # Dataset definitions
-    dataset_train = OvaryDataset(im_dir='../dataset/im/train_/', gt_dir='../dataset/gt/train/', transform=transform, imap=interaction, clahe=clahe)
-    dataset_val = OvaryDataset(im_dir='../dataset/im/val/', gt_dir='../dataset/gt/val/', imap=interaction, clahe=clahe)
-    dataset_test = OvaryDataset(im_dir='../dataset/im/test/', gt_dir='../dataset/gt/test/', imap=interaction, clahe=clahe)
+    dataset_train = OvaryDataset(im_dir='../dataset/im/train_/',gt_dir='../dataset/gt/train/',  imap=interaction, clahe=clahe, transform=transform)
+    dataset_val =   OvaryDataset(im_dir='../dataset/im/val/',   gt_dir='../dataset/gt/val/',    imap=interaction, clahe=clahe)
+    dataset_test =  OvaryDataset(im_dir='../dataset/im/test/',  gt_dir='../dataset/gt/test/',   imap=interaction, clahe=clahe)
 
     # Training Parameters
     if opt == 'adam':
